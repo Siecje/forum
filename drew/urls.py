3eeaaa690097eda872b7c5af22379273a23bcc8e
Cody Scott <cody.j.b.scott@gmail.com>
2013-12-03 02:28:05 -0500

    Adding Views and Templates

--- drew/urls.py
+++ drew/urls.py
@@ -10,6 +10,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'drew.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.home, name='home'),
-
+    url(r'^forum/', include('dequorum.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
